4f9f52067080730bc0a477a54b3f515cfa7fbddc
Pavel <developerayabol2009@gmail.com>
2026-03-14 19:58:38 +0500

    change variables to class

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-name = "John"
-age = 25
-print(f"Name: {name}, age: {age}")
+class Car:
+      def __init__(self, *, brand: str, model: str, price: int) -> None:
+            self.brand = brand
+            self.model = model
+            self.price = price
+            self.discount = self.discount
+
+      def __str__(self):
+            print(f"Car: {self.brand}, model: {self.model}, price: {self.price}")
+
+
+car1 = Car(brand="Audi", model="RS7", price=150000)
+print(car1)
